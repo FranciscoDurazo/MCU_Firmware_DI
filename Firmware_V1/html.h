7268b2d762b9f0746fd4dfe61fb75660f99f02ea
Francisco Durazo <fcodl2001@gmail.com>
2022-11-17 04:45:00 -0800

    Created file for webserver Firmware

---
+++ Firmware_V1/html.h
@@ -0,0 +1,40 @@
+//HTML code---------------------
+String html = R"***(
+<!DOCTYPE html>
+<html>
+<head>
+	    <title>E S P 8 2 6 6</title>
+</head>
+
+<!--HTML body formatting-->
+<style>
+	    body {font-family: "Arial"; background-color: yellow}
+      h1   {color: blue; text-align:center; font-size: 80px}
+	    h2   {color: black; font-size: 40px}
+	    h3   {color: red; text-align:center; font-size: 20px}
+</style>
+<!--end of formatting-->
+
+<body>
+	<h1>
+		  E S P 8 2 6 6<br>
+		  Microcontroller<br>
+  </h1>
+	<h2>
+		  <a href="https://en.wikipedia.org/wiki/ESP8266">Specifications:</a><br>     
+      <ul>
+          <li>32-bit controller @ 80 MHz<br></li>
+          <li>Has on-chip WiFi module<br></li>
+          <li>Applications: Webserver & IoT<br></li>
+     </ul>
+     <center>
+          ESP8266 pinouts<br><br>
+          <img src="https://i2.wp.com/randomnerdtutorials.com/wp-content/uploads/2019/05/ESP8266-NodeMCU-kit-12-E-pinout-gpio-pin.png?quality=100&strip=all&ssl=1"><br>
+	   </center>
+	</h2>
+	<h3>
+		  <p>Anas Kuzechie, Dec 2020</p>
+	</h3>
+</body>
+</html>
+)***";
