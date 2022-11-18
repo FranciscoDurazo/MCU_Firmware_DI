8e39ab6a3a2e8875e953aaf4f37b9592aaea3dcc
Francisco Durazo <fcodl2001@gmail.com>
2022-11-17 19:00:37 -0800

    Firmware V2 -> webserver

--- Firmware_V2/Firmware_V2.ino
+++ Firmware_V2/Firmware_V2.ino
@@ -1,9 +1,50 @@
-void setup() {
-  // put your setup code here, to run once:
 
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+//---------------------------------
+#include "html.h"                       //header file containing HTML code
+//---------------------------------
+const char* ssid     = "CETYS_Universidad";
+int led = 5;
+//const char* password = "password";
+ESP8266WebServer server(80);            //pointer to communicate with client via port 80
+//---------------------------------
+
+void handleRoot()
+{
+  server.send(200, "text/html", html);
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void ledon()
+{
+  digitalWrite(led, 1);
+  server.send(200, "text/html", html);
+}
+void ledoff()
+{
+  digitalWrite(led, 0);
+  server.send(200, "text/html", html);
+}
+void setup()
+{
+  pinMode(led,OUTPUT);
+  Serial.begin(115200);
+  WiFi.begin(ssid);           //connect to Wi-Fi network with SSID and password
+  Serial.print("\n\r \n\rWorking to connect");
+  while (WiFi.status() != WL_CONNECTED) {delay(500); Serial.print(".");}
+  Serial.println("");
+  Serial.println("ESP8266 Webpage");
+  Serial.println("Connected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", handleRoot);           //initialize web server
+  server.on("/ledon", ledon);
+  server.on("/ledoff", ledoff);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+//==============================================================
+void loop()
+{
+  server.handleClient();                //server continously looks for client request
 }
---
+++ Firmware_V2/html.h
@@ -0,0 +1,30 @@
+//HTML code---------------------
+String html = R"***(
+<!DOCTYPE html>
+<html>
+<head>
+	    <title>E S P 8 2 6 6</title>
+</head>
+
+<!--HTML body formatting-->
+<style>
+	    body {font-family: "Arial"; background-color: black}
+      h1   {color: white; text-align:center; font-size: 40px}
+	    h2   {color: black; font-size: 40px}
+	    h3   {color: red; text-align:center; font-size: 20px}
+      p   {color: white; font-size: 40px}
+</style>
+<!--end of formatting-->
+
+<body>
+	<h1>
+		  E S P 8 2 6 6<br>
+		  Microcontroller<br>
+  </h1>
+	<h1>SWITCH LED</h1>
+    <p><a href=/ledon>LED on</a></p>
+    <p><a href=/ledoff>LED off</a></p>
+    
+</body>
+</html>
+)***";
